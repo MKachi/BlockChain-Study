c14ef35d215d342e48b2829d633c4ad29b84a9aa
맛카치 <mkachi@naver.com>
2018-07-10 21:53:53 +0900

    Write server.js

--- CoinJS/server.js
+++ CoinJS/server.js
@@ -1,1 +1,13 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const blockChain = require("./blockchain");
+
+const { getBlockChain, createBlock } = blockChain;
+const PORT = 3000;
+const app = express();
+app.use(bodyParser.json());
+app.use(morgan("combined"));
+app.listen(PORT, function() {
+	console.log("Coin server running on " + PORT);
+});
