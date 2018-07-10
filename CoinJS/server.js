64ec5c8e95404abfb37e98887a3a56c1deca704a
맛카치 <mkachi@naver.com>
2018-07-10 22:26:15 +0900

    Change PORT to env variable

--- CoinJS/server.js
+++ CoinJS/server.js
@@ -4,7 +4,10 @@ const morgan = require("morgan");
 const blockChain = require("./blockchain");
 
 const { getBlockChain, createBlock } = blockChain;
-const PORT = 3000;
+
+// process환경설정에서 PORT를 찾고 이게 없다면 3000으로 실행됨
+// 콘솔에서 export PORT=3000 과 같이 환경설정 가능
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(bodyParser.json());
